9a1e317c484983387a94d88ca85eb3b2b86381cb
dipankar_c <dc954725@gmail.com>
2025-07-02 21:43:27 +0530

    saving changes

--- src/main/java/com/naukri/base/BaseTest.java
+++ src/main/java/com/naukri/base/BaseTest.java
@@ -7,10 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
-import java.io.File;
 import java.time.Duration;
-import java.util.UUID;
 
 public class BaseTest {
     protected static WebDriver driver;
@@ -22,42 +19,27 @@ public class BaseTest {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
 
-            // Create unique temp directory for user data
-            String tempDir = System.getProperty("java.io.tmpdir");
-            String userDataDir = tempDir + File.separator + "chrome-user-data-" + UUID.randomUUID();
-            new File(userDataDir).mkdirs();
-
-            // Basic Chrome options
+            // Basic options that work in both local and CI
             options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
             options.addArguments("--disable-extensions");
             options.addArguments("--disable-notifications");
-            options.addArguments("--start-maximized");
-
-            // Set unique user data directory
-            options.addArguments("--user-data-dir=" + userDataDir);
-
-            // Additional CI-specific options
-            if (System.getenv("CI") != null) {
-                options.addArguments("--headless=new");
-                options.addArguments("--window-size=1920,1080");
-                options.addArguments("--disable-setuid-sandbox");
-                options.addArguments("--disable-infobars");
-                options.addArguments("--disable-logging");
-                options.addArguments("--disable-popup-blocking");
-            }
 
             try {
                 driver = new ChromeDriver(options);
-                driver.manage().window().maximize();
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
                 driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-                driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
                 wait = new WebDriverWait(driver, Duration.ofSeconds(30));
             } catch (Exception e) {
                 System.err.println("Failed to initialize WebDriver: " + e.getMessage());
+                if (driver != null) {
+                    driver.quit();
+                }
                 throw e;
             }
         }
@@ -66,13 +48,8 @@ public class BaseTest {
     @AfterClass
     public void tearDown() {
         if (driver != null) {
-            try {
-                driver.quit();
-            } catch (Exception e) {
-                System.err.println("Error during driver quit: " + e.getMessage());
-            } finally {
-                driver = null;
-            }
+            driver.quit();
+            driver = null;
         }
     }
 }
