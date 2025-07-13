bf34e60533b4cdb6f0d65d30d279ac05d95580b4
dipankar_c <dc954725@gmail.com>
2025-07-13 12:10:14 +0530

    resolved the yml issues...

--- src/test/java/com/naukri/runners/TestRunner.java
+++ src/test/java/com/naukri/runners/TestRunner.java
@@ -5,12 +5,16 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "classpath:features", //"src/test/resources/features",
         glue = "com.naukri.stepdefinitions",
         plugin = {
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty.html",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+                /*"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "html:target/cucumber-reports/report.html",
-                "json:target/cucumber-reports/report.json"
+                "json:target/cucumber-reports/report.json"*/
         },
         monochrome = true
 )
