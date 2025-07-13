bd14d8cfd0163eeae4e1747dc14e6133980a74a9
dipankar_c <dc954725@gmail.com>
2025-07-13 20:29:56 +0530

    LoginPage is update with proper waits for CI/CD..._01

--- src/main/java/com/naukri/pages/LoginPage.java
+++ src/main/java/com/naukri/pages/LoginPage.java
@@ -14,7 +14,7 @@ public class LoginPage extends BasePage {
     private WebDriver driver;
 
     // Locators
-    private By usernameField = By.xpath("//input[@id='usernameField' and @type='text']"); //By.id("usernameField");
+    private By usernameField = By.cssSelector("#usernameField"); //By.xpath("//input[@id='usernameField' and @type='text']"); //By.id("usernameField");
     private By passwordField = By.xpath("//input[@id='passwordField' and @type='password']"); //By.id("passwordField");
     private By loginButton = By.xpath("//button[text()='Login']");
     private By resumeManagementLink = By.id("resumeManagement");
