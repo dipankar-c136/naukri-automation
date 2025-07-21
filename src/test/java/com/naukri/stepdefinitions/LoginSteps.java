c08ca36c201c474055b0d5040531aa7f69731111
dipankar_c <dc954725@gmail.com>
2025-07-21 22:02:44 +0530

    LoginPage is update with proper waits for CI/CD..._02

--- src/test/java/com/naukri/stepdefinitions/LoginSteps.java
+++ src/test/java/com/naukri/stepdefinitions/LoginSteps.java
@@ -69,6 +69,8 @@ public class LoginSteps extends BaseTest {
         getDriver().get("https://www.naukri.com/nlogin/login");
         getDriver().manage().window().maximize();
         Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
     }
 
     @When("I enter my credentials and login")
